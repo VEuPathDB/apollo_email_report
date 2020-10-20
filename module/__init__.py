f7cf6edcc4d1eec0e5b3249e4243b651d95e136d
Mikkel Christensen <mikkel@ebi.ac.uk>
2020-10-20 12:19:27 +0100

    adding copyright

--- module/__init__.py
+++
@@ -1,5 +0,0 @@
-from module import annotation_quality_report
-from module import annotator
-from module import gff_file
-from module import transcript
-from module import validation_error
--- module/validation_error.py
+++ module/validation_error.py
@@ -1,3 +1,18 @@
+"""
+Copyright [2017-2020] EMBL-European Bioinformatics Institute
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+     http://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+
+
 class ValidationError:
     def __init__(self, owner, organism_name, gene_id, mrna_id=None, gene_name=None, locus=None):
         self.owner = owner
